558fbf80ffb95fd4160de8bf98ca8b9ccc5f950c
jackshawn <jackshawn@live.com>
2017-11-06 21:51:02 +0800

    update pics

--- pages/help/help.js
+++ pages/help/help.js
@@ -1,12 +1,7 @@
 Page({
   data: {
     imgUrls: [
-      '../../images/1.PNG',
-      '../../images/2.PNG',
-      '../../images/3.PNG',
-      '../../images/4.PNG',
-      '../../images/5.PNG',
-      '../../images/6.PNG',
+'https://raw.githubusercontent.com/jackshawn/chideqi/master/images/1.PNG',             'https://raw.githubusercontent.com/jackshawn/chideqi/master/images/2.PNG', 'https://raw.githubusercontent.com/jackshawn/chideqi/master/images/3.PNG', 'https://raw.githubusercontent.com/jackshawn/chideqi/master/images/4.PNG', 'https://raw.githubusercontent.com/jackshawn/chideqi/master/images/5.PNG',          'https://raw.githubusercontent.com/jackshawn/chideqi/master/images/6.PNG',
     ],
     indicatorDots: true,
     duration: 400
