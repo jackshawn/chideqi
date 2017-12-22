9b1667b58365e07dcc91a00baf0a63537e9739b9
jackshawn <jackshawn@live.com>
2017-12-22 10:19:51 +0800

    format and update code

--- pages/help/help.js
+++ pages/help/help.js
@@ -1,10 +1,10 @@
 Page({
-  data: {
-    imgUrls: [
-      'https://raw.githubusercontent.com/jackshawn/chideqi/master/images/1.PNG', 'https://raw.githubusercontent.com/jackshawn/chideqi/master/images/2.PNG', 'https://raw.githubusercontent.com/jackshawn/chideqi/master/images/3.PNG', 'https://raw.githubusercontent.com/jackshawn/chideqi/master/images/4.PNG', 'https://raw.githubusercontent.com/jackshawn/chideqi/master/images/5.PNG', 'https://raw.githubusercontent.com/jackshawn/chideqi/master/images/6.PNG',
-    ],
-    indicatorDots: true,
-    duration: 400,
-    autoplay: true
-  }
+	data: {
+		imgUrls: [
+			'https://raw.githubusercontent.com/jackshawn/chideqi/master/images/1.PNG', 'https://raw.githubusercontent.com/jackshawn/chideqi/master/images/2.PNG', 'https://raw.githubusercontent.com/jackshawn/chideqi/master/images/3.PNG', 'https://raw.githubusercontent.com/jackshawn/chideqi/master/images/4.PNG', 'https://raw.githubusercontent.com/jackshawn/chideqi/master/images/5.PNG', 'https://raw.githubusercontent.com/jackshawn/chideqi/master/images/6.PNG',
+		],
+		indicatorDots: true,
+		duration: 400,
+		autoplay: true
+	}
 })
