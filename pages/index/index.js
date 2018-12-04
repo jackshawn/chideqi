221a6986de631f844df4acd5ba372529d4c3e146
jackshawn <jackshawn@live.com>
2018-12-04 12:04:38 +0800

    删除多查询, 重新配置cookie

--- pages/index/index.js
+++ pages/index/index.js
@@ -17,7 +17,7 @@ Page({
 			'Referer': 'https://h5.ele.me/hongbao/',
 			'X-Shard': 'eosid=3014974170551246000',
 			// 'Connection': 'keep-alive',
-			'Cookie': 'ubt_ssid=xc0zu3vnw3pckza0c0r1fswe4kwve83h_2017-11-02; perf_ssid=8rldm86hl1qbb0jjbcmbvvflzxekqzjc_2017-11-02; _utrace=8bb8b70ae0c1ad1a38b295cf20d7d97b_2017-11-02; eleme__ele_me=408cb6e4447104b9b2af12dfbb330be1%3A2e5c5e6dc1afb137d7b004aaccc90af32761f9b7; track_id=1509610361%7Cd047555ab922ad0017b8d3089174e214cdaf88144d5c3f2cb9%7Cb9f05b492a4754c71f984a0492652649; snsInfo=%7B%22city%22%3A%22%22%2C%22eleme_key%22%3A%224e912de2215b79fc23675d313f0dc018%22%2C%22figureurl%22%3A%22http%3A%2F%2Fqzapp.qlogo.cn%2Fqzapp%2F101204453%2F642E8E48EC04337A8E07035EF65F9D1F%2F30%22%2C%22figureurl_1%22%3A%22http%3A%2F%2Fqzapp.qlogo.cn%2Fqzapp%2F101204453%2F642E8E48EC04337A8E07035EF65F9D1F%2F50%22%2C%22figureurl_2%22%3A%22http%3A%2F%2Fqzapp.qlogo.cn%2Fqzapp%2F101204453%2F642E8E48EC04337A8E07035EF65F9D1F%2F100%22%2C%22figureurl_qq_1%22%3A%22http%3A%2F%2Fq.qlogo.cn%2Fqqapp%2F101204453%2F642E8E48EC04337A8E07035EF65F9D1F%2F40%22%2C%22figureurl_qq_2%22%3A%22http%3A%2F%2Fq.qlogo.cn%2Fqqapp%2F101204453%2F642E8E48EC04337A8E07035EF65F9D1F%2F100%22%2C%22gender%22%3A%22%E7%94%B7%22%2C%22is_lost%22%3A0%2C%22is_yellow_vip%22%3A%220%22%2C%22is_yellow_year_vip%22%3A%220%22%2C%22level%22%3A%220%22%2C%22msg%22%3A%22%22%2C%22nickname%22%3A%22%E4%BC%8A%E4%B8%B6%E6%97%A0%E6%89%80%E6%9C%89%22%2C%22openid%22%3A%22642E8E48EC04337A8E07035EF65F9D1F%22%2C%22province%22%3A%22%E6%B1%9F%E8%8B%8F%22%2C%22ret%22%3A0%2C%22vip%22%3A%220%22%2C%22year%22%3A%221991%22%2C%22yellow_vip_level%22%3A%220%22%2C%22name%22%3A%22%E4%BC%8A%E4%B8%B6%E6%97%A0%E6%89%80%E6%9C%89%22%2C%22avatar%22%3A%22http%3A%2F%2Fq.qlogo.cn%2Fqqapp%2F101204453%2F642E8E48EC04337A8E07035EF65F9D1F%2F40%22%7D'
+			'Cookie': 'perf_ssid=qlj904fr2gcm5iwbn59yvunj8p0xvjiu_2018-12-04; ubt_ssid=3bi4ktdmeujdgbohyx2m8c8mz2ofu1sg_2018-12-04; _utrace=2bbeedce388099116dbd4a0d17aa92ab_2018-12-04; snsInfo[101204453]=%7B%22city%22%3A%22%22%2C%22constellation%22%3A%22%22%2C%22eleme_key%22%3A%224e912de2215b79fc23675d313f0dc018%22%2C%22figureurl%22%3A%22http%3A%2F%2Fqzapp.qlogo.cn%2Fqzapp%2F101204453%2F642E8E48EC04337A8E07035EF65F9D1F%2F30%22%2C%22figureurl_1%22%3A%22http%3A%2F%2Fqzapp.qlogo.cn%2Fqzapp%2F101204453%2F642E8E48EC04337A8E07035EF65F9D1F%2F50%22%2C%22figureurl_2%22%3A%22http%3A%2F%2Fqzapp.qlogo.cn%2Fqzapp%2F101204453%2F642E8E48EC04337A8E07035EF65F9D1F%2F100%22%2C%22figureurl_qq_1%22%3A%22http%3A%2F%2Fthirdqq.qlogo.cn%2Fqqapp%2F101204453%2F642E8E48EC04337A8E07035EF65F9D1F%2F40%22%2C%22figureurl_qq_2%22%3A%22http%3A%2F%2Fthirdqq.qlogo.cn%2Fqqapp%2F101204453%2F642E8E48EC04337A8E07035EF65F9D1F%2F100%22%2C%22gender%22%3A%22%E7%94%B7%22%2C%22is_lost%22%3A0%2C%22is_yellow_vip%22%3A%220%22%2C%22is_yellow_year_vip%22%3A%220%22%2C%22level%22%3A%220%22%2C%22msg%22%3A%22%22%2C%22nickname%22%3A%22%E4%BC%8A%E4%B8%B6%E6%97%A0%E6%89%80%E6%9C%89%22%2C%22openid%22%3A%22642E8E48EC04337A8E07035EF65F9D1F%22%2C%22province%22%3A%22%E6%B1%9F%E8%8B%8F%22%2C%22ret%22%3A0%2C%22vip%22%3A%220%22%2C%22year%22%3A%221991%22%2C%22yellow_vip_level%22%3A%220%22%2C%22name%22%3A%22%E4%BC%8A%E4%B8%B6%E6%97%A0%E6%89%80%E6%9C%89%22%2C%22avatar%22%3A%22http%3A%2F%2Fthirdqq.qlogo.cn%2Fqqapp%2F101204453%2F642E8E48EC04337A8E07035EF65F9D1F%2F40%22%7D; track_id=1543893764|7dc68447f2a13210a0aa2868283fa4aaa44040e72747672556|febb3d751492b9b8a4339418b6deb6e0; USERID=4915166426; SID=fZalvlMJnHhh9PROoFl71y4clK75iq3fhKTA'
 		},
 		luckyNum: 0,
 		users: []
@@ -29,16 +29,7 @@ Page({
 	getUrl: function (e) {
 		var url = e.detail.value;
 		console.log(url)
-		if (url.indexOf('meituan') !== -1) {
-			console.log('meituan')
-			wx.setNavigationBarColor({
-				frontColor: '#ffffff',
-				backgroundColor: '#f0b209'
-			})
-			this.setData({
-				color: '#f0b209'
-			});
-		} else if (url.indexOf('ele.me') !== -1) {
+        if (url.indexOf('ele.me') !== -1) {
 			wx.setNavigationBarColor({
 				frontColor: '#ffffff',
 				backgroundColor: '#3da8f9'
@@ -64,23 +55,17 @@ Page({
 		var sn = (function () {
 			var r, from, to;
 			var url = _this.data.url;
-			if (url.indexOf('meituan') !== -1) {
-				from = url.indexOf('urlKey=');
-				if (from !== -1) {
-					r = url.substr(from + 7, 32)
-				}
-			} else {
-				from = url.indexOf('&sn=');
-				to = url.indexOf('&theme_id');
-				if (from !== -1) {
-					r = url.slice(from + 4, to)
-				}
-				
-				// luckuy num
-				_this.setData({
-					luckyNum: url.slice(url.indexOf('&lucky_number=') + 14, url.indexOf('&track_id'))
-				})
-			}
+            from = url.indexOf('&sn=');
+            to = url.indexOf('&theme_id');
+            if (from !== -1) {
+                r = url.slice(from + 4, to)
+            }
+
+            // luckuy num
+            _this.setData({
+                luckyNum: url.slice(url.indexOf('&lucky_number=') + 14, url.indexOf('&track_id'))
+            });
+
 			return r;
 		})()
 		
@@ -90,59 +75,39 @@ Page({
 				btnDisabled: true
 			});
 			try {
-				if (sn.length === 32) {
-					wx.request({
-						url: 'https://xiang.pics/getEleNum?sn=' + sn,
-						method: 'GET',
-						success: function (res) {
-							console.log(res)
-							_this.setData({
-								btnName: '查 询',
-								btnDisabled: false,
-								needHelp: false
-							})
-							wx.showModal({
-								title: '提示',
-								content: res.data.result,
-								showCancel: false
-							})
-						}
-					})
-				} else {
-					wx.request({
-						url: 'https://restapi.ele.me/marketing/promotion/weixin/642E8E48EC04337A8E07035EF65F9D1F',
-						method: 'POST',
-						header: _this.data.elemeHeader,
-						data: _this.getData(sn),
-						success: function (res) {
-							console.log(res.data)
-							if (res.data.message) {
-								_this.setData({
-									btnName: '查 询',
-									btnDisabled: false,
-									needHelp: false
-								})
-								wx.showToast({
-									title: res.data.message
-								})
-							} else {
-								res.data.promotion_records.forEach(i => {
-									i.time = new Date(i.created_at).toString().substr(16,8)
-								})
-								_this.setData({
-									btnName: '查 询',
-									btnDisabled: false,
-									needHelp: false,
-									users: res.data.promotion_records
-								});
-								
-								wx.showToast({
-									title: '查询成功!'
-								})
-							}
-						}
-					})
-				}
+                wx.request({
+                    url: 'https://restapi.ele.me/marketing/promotion/weixin/642E8E48EC04337A8E07035EF65F9D1F',
+                    method: 'POST',
+                    header: _this.data.elemeHeader,
+                    data: _this.getData(sn),
+                    success: function (res) {
+                        console.log(res.data)
+                        if (res.data.message) {
+                            _this.setData({
+                                btnName: '查 询',
+                                btnDisabled: false,
+                                needHelp: false
+                            })
+                            wx.showToast({
+                                title: res.data.message
+                            })
+                        } else {
+                            res.data.promotion_records.forEach(i => {
+                                i.time = new Date(i.created_at).toString().substr(16,8)
+                        })
+                            _this.setData({
+                                btnName: '查 询',
+                                btnDisabled: false,
+                                needHelp: false,
+                                users: res.data.promotion_records
+                            });
+
+                            wx.showToast({
+                                title: '查询成功!'
+                            })
+                        }
+                    }
+                })
 			} catch (e) {
 				console.log(e)
 			}
